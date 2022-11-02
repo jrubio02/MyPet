84c6ef390dd44ca2537e51971b09c441287ab06b
Jorge Rubio <jrubiosantiago@gmail.com>
2022-11-02 05:20:44 -0700

    .

---
+++ probando/app/src/main/java/com/example/probando/FLogin.java
@@ -0,0 +1,30 @@
+package com.example.probando;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class FLogin extends AppCompatActivity {
+    public Button button;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_flogin);
+
+        button = (Button) findViewById(R.id.enlace_registro);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(FLogin.this,FRegister.class);
+                startActivity(intent);
+            }
+        });
+
+
+
+    }
+}
---
+++ probando/app/src/main/java/com/example/probando/FRegister.java
@@ -0,0 +1,79 @@
+package com.example.probando;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.probando.db.DbContactos;
+
+public class FRegister extends AppCompatActivity {
+
+    EditText txtNombre, txtEmail, txtPassword, txtPhone;
+
+    public Button button;
+    public Button btnRegistro;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fregister);
+
+
+        button = (Button) findViewById(R.id.enlace_registro);
+        txtNombre = findViewById(R.id.txtNombre);
+        txtEmail = findViewById(R.id.txtEmail);
+        txtPassword = findViewById(R.id.txtPassword);
+        txtPhone = findViewById(R.id.txtPhone);
+
+        btnRegistro = (Button) findViewById(R.id.btnRegistro);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(FRegister.this, FLogin.class);
+                startActivity(intent);
+            }
+
+
+        });
+
+        btnRegistro.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                DbContactos dbContactos = new DbContactos(FRegister.this);
+                long id = dbContactos.insertarUsuario(
+                        txtNombre.getText().toString(),
+                        txtEmail.getText().toString(),
+                        txtPhone.getText().toString(),
+                        txtPassword.getText().toString());
+
+
+                if (id > 0) {
+                    Toast.makeText(FRegister.this, "REGISTRO GUARDADO", Toast.LENGTH_LONG).show();
+                    Log.d(txtNombre.getText().toString(), "");
+                    limpiar();
+                } else {
+                    Toast.makeText(FRegister.this, "ERROR AL GUARDAR", Toast.LENGTH_LONG).show();
+
+                }
+
+
+            }
+        });
+    }
+
+    private void limpiar()
+    {
+        txtNombre.setText("");
+        txtEmail.setText("");
+        txtPassword.setText("");
+        txtPhone.setText("");
+    }
+
+}
